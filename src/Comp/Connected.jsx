5b0f1b04618a2d3fadb9ccf67e59754e438ad3c2
taamzid <47922615+taamzid@users.noreply.github.com>
2024-01-09 21:21:22 +0600

    Update Connected.jsx

--- src/Comp/Connected.jsx
+++ src/Comp/Connected.jsx
@@ -5,7 +5,7 @@ import RightArrow from "../assets/RightArrow.svg";
 
 const Connected = () => {
   return (
-    <div className="pt-[80px] lg:pt-[100px] pb-[80px] lg:pb-[120px] bg-[linear-gradient(180deg,_#dedede00_0%,_#174aff4d_100%)] rounded-[30px] lg:bg-none bg-cover">
+    <div className="relative pt-[80px] lg:pt-[100px] pb-[80px] lg:pb-[120px] bg-[linear-gradient(180deg,_#dedede00_0%,_#174aff4d_100%)] rounded-[30px] lg:bg-none bg-cover">
       <div className="px-[16px] block lg:hidden text-center text-[14px] lg:text-[20px] font-normal text-[#9A9AB5] font-['Sharp-Grotesk-Medium']">
         <h1>
           In today’s fast-paced digital world, the difference between thriving
@@ -31,19 +31,25 @@ const Connected = () => {
       <div className="hidden lg:block lg:flex items-center justify-center lg:mt-[80px]">
         <img src={connectedImage} alt="connectedImage" />
       </div>
-      <div className="block hidden px-[38px]">
-        <div className="absolute top-[38%] font-['Poppins'] overflow-hidden bg-[#fff] px-[24px] py-[40px]">
+      <div className="px-[38px]">
+        <div className="absolute tabsolute top-[50%] lg:top-[58%] left-1/2 transform -translate-x-[100px] lg:-translate-x-[600px] -translate-y-1/2 font-['Poppins'] overflow-hidden px-[24px] py-[40px]">
           <div>
             <h1 className="text-[#292930] text-[32px] font-medium">
               Why EvolvePro.AI?
             </h1>
           </div>
           <div className="lg:mt-[32px] text-[16px] text-[#999FAE] font-normal">
-            <h1>Imagine a world where every website visitor is greeted with the same enthusiasm and expertise as a face-to-face meeting.</h1>
+            <h1>Imagine a world where every website</h1>
+            <h1>visitor is greeted with the same</h1>
+            <h1>enthusiasm and expertise as a face-to-</h1>
+            <h1>face meeting.</h1>
             <br />
-            <h1>Where every query is met with insightful, personalised responses, and no sales opportunity slips through the cracks.</h1>
+            <h1>Where every query is met with insightful,</h1>
+            <h1>personalised responses, and no sales</h1>
+            <h1>opportunity slips through the cracks.</h1>
             <br />
-            <h1>This is the world EvolvePro.AI creates for your business.</h1>
+            <h1>This is the world EvolvePro.AI creates for</h1>
+            <h1>your business.</h1>
           </div>
           <div className="flex gap-[17px] lg:mt-[114px] ml-[8px]">
             <img src={leftArrow} alt="leftArrow" />
