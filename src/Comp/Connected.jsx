d75771f56b01214cd1f88ffb2af3be2e0758c21b
taamzid <47922615+taamzid@users.noreply.github.com>
2024-01-10 01:43:51 +0600

    Update Connected.jsx

--- src/Comp/Connected.jsx
+++ src/Comp/Connected.jsx
@@ -1,5 +1,5 @@
 import connectedImage from "../assets/connectedImage.svg";
-import connectedImageMobile from "../assets/connected-image-mobile.svg";
+import connectedImageMobile from "../assets/bg-collected-final.svg";
 import leftArrow from "../assets/LeftArrow.svg";
 import RightArrow from "../assets/RightArrow.svg";
 import { useState } from "react";
@@ -166,6 +166,116 @@ const Connected = () => {
       ),
     },
   ];
+  const slidesMobile = [
+    {
+      title: "Why EvolvePro.AI?",
+      content: (
+        <>
+          <h1>Imagine a world where every website visitor is greeted with the same enthusiasm and expertise as a face-to-face meeting.</h1>
+          <br />
+          <h1>Where every query is met with insightful, personalized responses, and no sales opportunity slips through the cracks.</h1>
+          <br />
+          <h1>This is the world EvolvePro.AI creates for your business.</h1>
+        </>
+      ),
+    },
+    {
+      title: ( 
+        <>
+          <h1>The Power of AI, The Touch</h1>
+          <h1>of Human Empathy</h1>
+        </>
+      ),
+      content: (
+        <>
+          <h1>EvolvePro.AI is not your average chatbot... It’s a sophisticated blend of artificial intelligence and a deep understanding of human interaction.</h1>
+          <h1 className="mt-[10px]">Our AI doesn’t just respond; it engages, learns, and adapts.</h1>
+          <h1 className="mt-[10px]">It’s like having a top-tier sales team that works tirelessly, 24/7, without ever losing its charm or efficiency.</h1>
+        </>
+      ),
+    },
+    {
+      title: ( 
+        <>
+          <h1>Tailored to Your Unique</h1> 
+          <h1>Business Needs</h1>
+        </>
+      ),
+      content: (
+        <>
+          <h1>Every business is unique, and so are its challenges.</h1>
+          <h1 className="mt-[10px]">EvolvePro.AI is designed to understand and adapt to your specific industry needs with our intuitive onboarding process.</h1>
+          <h1 className="mt-[10px]">Whether you’re in e-commerce, real estate, healthcare, or any other sector, our AI learns your business language and engages your customers as an industry insider.</h1>
+        </>
+      ),
+    },
+    {
+      title: ( 
+        <>
+          <h1>A Seamless Integration</h1>
+          <h1>Process</h1>
+        </>
+      ),
+      content: (
+        <>
+          <h1>Integrating EvolvePro.AI into your existing digital platform (website, sales funnel, landing pages) is incredibly simple.</h1>
+          <h1 className="mt-[10px]">It works harmoniously and integrates with any website, providing a seamless experience for both you and your customers.</h1>
+          <h1 className="mt-[10px]">It’s not just a chatbot; it’s the central nerve of your digital presence.</h1>
+        </>
+      ),
+    },
+    {
+      title: ( 
+        <>
+          <h1>Data-Driven Insights for</h1> 
+          <h1>Your Strategic Decisions.</h1>
+        </>
+      ),
+      content: (
+        <>
+          <h1>With EvolvePro.AI, every interaction is an dopportunity to learn.</h1>
+          <br />
+          <h1>Our AI provides real-time insights into customer behavior, preferences, and feedback, empowering you to make data-driven decisions that propel your business forward.</h1>
+        </>
+      ),
+    },
+    {
+      title: ( 
+        <>
+          <h1>Join the Evolution, Adopt</h1> 
+          <h1>Change Before your</h1>
+          <h1>Competitors...</h1>
+        </>
+      ),
+      content: (
+        <>
+          <h1>The future of business growth is here, and it’s powered by AI.</h1>
+          <br />
+          <h1>EvolvePro.AI is more than a tool; it’s a strategic partner that works tirelessly to bring you closer to your business goals.</h1>
+          <br />
+          <h1>Are you ready to evolve?</h1>
+        </>
+      ),
+    },
+    {
+      title: ( 
+        <>
+          <h1>Take the First Step...</h1> 
+          <h1>Start Your Free Trial</h1> 
+          <h1>Today!</h1>
+        </>
+      ),
+      content: (
+        <>
+          <h1>Experience the power of EvolvePro.AI with a FREE 14 Day Trial!</h1>
+          <br />
+          <h1>See firsthand how our AI can transform your customer engagement and bsupercharge your sales.</h1>
+          <br />
+          <h1>The future is waiting. Are you ready to be a part of it?</h1>
+        </>
+      ),
+    },
+  ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -198,7 +308,7 @@ const Connected = () => {
   const isLastSlide = currentSlide === slides.length - 1;
 
   return (
-    <div className="relative pt-[80px] lg:pt-[100px] pb-[80px] lg:pb-[120px] bg-[linear-gradient(180deg,_#dedede00_0%,_#174aff4d_100%)] rounded-[30px] lg:bg-none bg-cover">
+    <div className="relative pt-[80px] lg:pt-[100px] lg:pb-[120px]  rounded-[30px]  bg-cover">
       <div className="px-[16px] block lg:hidden text-center text-[14px] lg:text-[20px] font-normal text-[#9A9AB5] font-['Sharp-Grotesk-Medium']">
         <h1>
           In today’s fast-paced digital world, the difference between thriving
@@ -218,7 +328,7 @@ const Connected = () => {
         <h1>Staying connected with your</h1>
         <h1>prospects around the clock.</h1>
       </div>
-      <div className="px-[16px] block flex justify-center lg:hidden">
+      <div className="block flex justify-center lg:hidden">
         <img src={connectedImageMobile} alt="connectedImageMobile" />
       </div>
       <div className="hidden lg:block lg:flex items-center justify-center lg:mt-[80px]">
@@ -249,6 +359,46 @@ const Connected = () => {
         </div>
         ) : (
           <div className="flex gap-[17px] ml-[25px] absolute lg:top-[75%] left-1/2 transform -translate-x-[100px] lg:-translate-x-[610px] -translate-y-1/2"> 
+            <img
+              src={leftArrow}
+              alt="leftArrow"
+              onClick={handlePrevSlide}
+              className="cursor-pointer"
+            />
+            <img
+              src={RightArrow}
+              alt="RightArrow"
+              onClick={handleNextSlide}
+              className="cursor-pointer"
+            />
+          </div>
+        )}
+      </div>
+      <div className="px-[38px] lg:hidden block">
+        <div className="absolute top-[] left-[50%] transform -translate-x-1/2 translate-y-[-1120px] font-['Poppins'] overflow-hidden min-w-[260px]">
+          <div>
+            <h1 className="text-[#292930] text-[20px] font-medium leading-[25px]">
+            {slidesMobile[currentSlide].title}
+            </h1>
+          </div>
+          <div className="mt-[12px] text-[16px] text-[#999FAE] font-normal">
+          {slidesMobile[currentSlide].content}
+          </div>
+        </div>
+        {isLastSlide ? (
+        <div className="flex gap-[8px] absolute top-[58%] left-[18%]">
+          <img
+                src={leftArrow}
+                alt="leftArrow"
+                onClick={handlePrevSlide}
+                className="cursor-pointer"
+          />
+          <button onClick={handleNextSlide} className="bg-[#3662FE] text-[14px] text-[#fff] px-[14px] py-[10px] rounded-[25px]">
+            Start Your Free Trial Today
+          </button>
+        </div>
+        ) : (
+          <div className="flex gap-[17px] absolute top-[58%] left-[18%]"> 
             <img
               src={leftArrow}
               alt="leftArrow"
